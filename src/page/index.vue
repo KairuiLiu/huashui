2980a268117cd4cc4a338aafcc37dc01133afa50
LiuKairui <tclkr@live.com>
2022-02-10 21:54:01 +0800

    fix: pageFile

---
+++ src/page/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<router-view></router-view>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+</script>
+
+<script lang="ts">
+export default defineComponent({
+	name: 'IndexPage',
+});
+</script>
+
+<style lang="less" scoped></style>
